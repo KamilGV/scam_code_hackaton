f373e0c8e762f84be4311b66e60b841a1c7cdfdb
dezzer <baev2m@gmail.com>
2023-11-24 17:23:04 +0500

    fixf orm

--- frontend/src/components/AppLayout.tsx
+++ frontend/src/components/AppLayout.tsx
@@ -6,7 +6,7 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Container mih='100vh' miw='100vw' p='0' m='0' style={{ display: 'flex', flexDirection: 'column' }}>
       <Header />
-      <Box miw='100%' mih='100%' style={{ flex: '1' }}>
+      <Box miw='100%' mih='100%' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flex: '1' }}>
         {children}
       </Box>
     </Container>
--- frontend/src/features/courses/routes/CatalogPage.tsx
+++ frontend/src/features/courses/routes/CatalogPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+
+
 const CatalogPage = () => {
   return (
     <div>CatalogPage</div>
